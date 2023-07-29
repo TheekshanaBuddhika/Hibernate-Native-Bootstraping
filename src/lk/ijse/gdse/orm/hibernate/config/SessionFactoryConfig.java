5fd9bed9114fd0e03ca6f7c83e9f32b9e5c743b7
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2023-07-29 13:40:30 +0530

    hibernate lib added

--- src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
+++ src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
@@ -1,4 +1,19 @@
 package lk.ijse.gdse.orm.hibernate.config;
 
 public class SessionFactoryConfig {
+
+    private static SessionFactoryConfig factoryConfig;
+
+    private SessionFactoryConfig() {
+    }
+
+    public static SessionFactoryConfig getInstance() {
+//        if(null==factoryConfig){
+//            return factoryConfig = new SessionFactoryConfig();
+//        }
+//        return factoryConfig;
+        return (null==factoryConfig) ? factoryConfig = new SessionFactoryConfig() : factoryConfig ;
+    }
+
+
 }
