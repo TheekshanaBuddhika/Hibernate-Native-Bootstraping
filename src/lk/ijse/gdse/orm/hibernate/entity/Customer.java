a21809fdc6db80dc789ff4ca2fe40dcb3055ccc7
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2023-07-29 14:36:12 +0530

    mapping establish

--- src/lk/ijse/gdse/orm/hibernate/entity/Customer.java
+++ src/lk/ijse/gdse/orm/hibernate/entity/Customer.java
@@ -1,4 +1,27 @@
 package lk.ijse.gdse.orm.hibernate.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/*@Entity(name = "customer")*/
+
+@Entity
+@Table(name = "customer")
 public class Customer {
+
+    @Id
+    @Column(name = "customer_id")
+    private int id;
+
+    @Column(name = "customer_name")
+    private String name;
+
+    @Column(name = "customer_address")
+    private String address;
+
+    @Column(name = "customer_salary")
+    private Double salary;
+
 }
