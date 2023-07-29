424a6739a4a18f52886e333f6cd100470e359ca2
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2023-07-29 14:45:14 +0530

    "entity updated"

--- src/lk/ijse/gdse/orm/hibernate/entity/Customer.java
+++ src/lk/ijse/gdse/orm/hibernate/entity/Customer.java
@@ -24,4 +24,55 @@ public class Customer {
     @Column(name = "customer_salary")
     private Double salary;
 
+    public Customer(int id, String name, String address, Double salary) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.salary = salary;
+    }
+
+    public Customer() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
