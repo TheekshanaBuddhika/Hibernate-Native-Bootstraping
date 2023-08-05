6c1943747ff7d3f7fc04a1a48f5723fa0812dfaf
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2023-08-05 13:54:10 +0530

    code simplifing

--- src/lk/ijse/gdse/orm/hibernate/AppInitializer.java
+++ src/lk/ijse/gdse/orm/hibernate/AppInitializer.java
@@ -1,11 +1,63 @@
 package lk.ijse.gdse.orm.hibernate;
 
 import lk.ijse.gdse.orm.hibernate.config.SessionFactoryConfig;
+import lk.ijse.gdse.orm.hibernate.entity.Customer;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.engine.transaction.internal.TransactionImpl;
 
 public class AppInitializer {
     public static void main(String[] args) {
+
+        //Save using save method
         Session session = SessionFactoryConfig.getInstance().getSession();
-        System.out.println(session);
+
+        Customer customer = new Customer();
+        customer.setId(1);
+        customer.setName("Kamal");
+        customer.setAddress("Galle");
+        customer.setSalary(25000.00);
+
+        Transaction transaction = session.beginTransaction();
+        session.save(customer);
+        transaction.commit();
+
+        session.close();
+
+        //Update using save method
+      /*  Session session1 = SessionFactoryConfig.getInstance().getSession();
+
+        Transaction transaction = session1.beginTransaction();
+        Customer customer1 = session1.get(Customer.class, 1);
+        customer1.setAddress("Matara");
+        customer1.setName("Nimal");
+        session1.save(customer1);
+
+        transaction.commit();
+        session1.close();
+*/
+
+        //Update using update method
+      /*  Session session2 = SessionFactoryConfig.getInstance().getSession();
+
+        Transaction transaction = session2.beginTransaction();
+        Customer customer2 = session2.get(Customer.class, 1);
+        customer2.setAddress("Hambantota");
+        customer2.setName("Amara");
+        session2.update(customer2);
+
+        transaction.commit();
+        session2.close();*/
+
+        //Delete using delete method
+      /*  Session session3 = SessionFactoryConfig.getInstance().getSession();
+
+        Transaction transaction = session3.beginTransaction();
+        Customer customer3 = session3.get(Customer.class, 1);
+        session3.delete(customer3);
+
+        transaction.commit();
+        session3.close();*/
+
     }
 }
--- src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
+++ src/lk/ijse/gdse/orm/hibernate/config/SessionFactoryConfig.java
@@ -1,5 +1,6 @@
 package lk.ijse.gdse.orm.hibernate.config;
 
+import lk.ijse.gdse.orm.hibernate.entity.Customer;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
@@ -7,30 +8,38 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
 public class SessionFactoryConfig {
 
     private static SessionFactoryConfig factoryConfig;
+    private static SessionFactory sessionFactory;
 
     private SessionFactoryConfig() {
+     /*   StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure().build();
+
+        Metadata metadata = new MetadataSources(serviceRegistry).addAnnotatedClass(Customer.class).getMetadataBuilder().applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE).build();
+
+//        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();*/
+        /* sessionFactory = new MetadataSources(new StandardServiceRegistryBuilder().configure().build())
+                            .addAnnotatedClass(Customer.class)
+                            .getMetadataBuilder()
+                            .applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE)
+                            .build()
+                            .buildSessionFactory();*/
+                sessionFactory = new Configuration().configure().addAnnotatedClass(Customer.class).buildSessionFactory();
     }
 
     public static SessionFactoryConfig getInstance() {
-//        if(null==factoryConfig){
-//            return factoryConfig = new SessionFactoryConfig();
-//        }
-//        return factoryConfig;
+/*        if(null==factoryConfig){
+            return factoryConfig = new SessionFactoryConfig();
+        }
+     return factoryConfig;
+   */
         return (null==factoryConfig) ? factoryConfig = new SessionFactoryConfig() : factoryConfig ;
     }
 
     public Session getSession(){
-        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure().build();
-
-        Metadata metadata = new MetadataSources(serviceRegistry).getMetadataBuilder().applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE).build();
-
-//        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
-
-        SessionFactory sessionFactory = metadata.buildSessionFactory();
 
         return sessionFactory.openSession();
 
