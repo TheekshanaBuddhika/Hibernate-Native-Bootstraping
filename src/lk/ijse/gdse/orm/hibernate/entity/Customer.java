ea09daa28f417c6b2faa23199ad1b7a0deb2e0db
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2023-08-12 13:54:05 +0530

    class updated

--- src/lk/ijse/gdse/orm/hibernate/entity/Customer.java
+++ src/lk/ijse/gdse/orm/hibernate/entity/Customer.java
@@ -1,4 +1,36 @@
 package lk.ijse.gdse.orm.hibernate.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "customer")
 public class Customer {
+
+    @Id
+    @Column(name = "customer_id")
+    private int id;
+
+    @Column(name = "customer_name")
+    private String name;
+
+    @Column(name = "customer_address")
+    private String Address;
+
+    @Column(name = "customer_age")
+    private int age;
+
+    @Column(name = "customer_salary")
+    private Double salary;
 }
