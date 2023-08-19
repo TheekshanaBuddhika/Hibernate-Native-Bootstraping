26e1f1f86582a225119929f5b5048ba1075d591c
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2023-08-19 11:06:33 +0530

    Transient State in life cycle

--- src/lk/ijse/gdse/orm/hibernate/lifeCycle/TransientState.java
+++ src/lk/ijse/gdse/orm/hibernate/lifeCycle/TransientState.java
@@ -1,10 +1,14 @@
 package lk.ijse.gdse.orm.hibernate.lifeCycle;
 
+import lk.ijse.gdse.orm.hibernate.config.SessionFactoryConfig;
 import lk.ijse.gdse.orm.hibernate.entity.Customer;
+import org.hibernate.Session;
 
 public class TransientState {
 
     public static void main(String[] args) {
+
+        Session session = SessionFactoryConfig.getInstance().getSession();
 
         //Transient State
         Customer customer = new Customer();
@@ -14,6 +18,12 @@ public class TransientState {
         customer.setSalary(33433.00);
         customer.setAge(12);
 
+        System.out.println(isExist(session, customer));
+
+    }
+    
+    private static String isExist(Session session,Customer customer){
+        return session.contains(customer) ? "it is transient":"it is not transient";
     }
 
 }
